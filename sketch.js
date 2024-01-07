2f2dec7452f2999976937067ee21839f520e5b6b
RalphAdi <153388309+RalphAdi@users.noreply.github.com>
2024-01-07 15:47:53 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,10 @@ let engine;
 let world;
 var ball;
 var ground;
+var wedge;
+var angle=60;
+var poly;
+
 
 function setup() {
   createCanvas(400,400);
@@ -24,6 +28,9 @@ function setup() {
      isStatic: true
    };
   
+  var ops={
+    isStatic:true
+  };
 
   ground = Bodies.rectangle(200,390,400,20,ground_options);
   World.add(world,ground);
@@ -31,6 +38,13 @@ function setup() {
   ball = Bodies.circle(100,10,20,ball_options);
   World.add(world,ball);
   
+  wall = Bodies.rectangle(300, 150, 70, 10, ground_options);
+  World.add(world,wall);
+  
+  wedge = Bodies.rectangle(100,200,100,20,ops);
+  World.add(world,wedge);
+  
+
   rectMode(CENTER);
   ellipseMode(RADIUS);
 }
@@ -41,8 +55,22 @@ function draw()
   background(51);
   Engine.update(engine);
   
+  Matter.Body.rotate(wedge,angle)
+  push();
+  translate(wedge.position.x,wedge.position.y);
+  rotate(angle);
+  rect(0,0,100,20);
+  pop();
+  
+  angle +=0.1;
+
   ellipse(ball.position.x,ball.position.y,20);
   rect(ground.position.x,ground.position.y,400,20);
  
-}
 
+
+  rect(wall.position.x,wall.position.y,70,20);
+
+  
+  
+}
